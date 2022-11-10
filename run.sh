3ab32169533b7ee19c0d4905bb46bffc6598ccda
Karol Baraniecki <karol@baraniecki.eu>
2022-11-10 22:17:14 +0100

    Make it possible to choose the url

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
-file=./$(date '+nextbike-%Y-%m-%d-%H:%M:%S.json')
-curl 'https://wroclawskirower.pl/mapa-stacji/' \
+#!/bin/bash
+set -e -o pipefail
+
+if [[ $# != 1 ]]; then
+	echo "Usage: $0 <url to public nextbike map>"
+	exit 1
+fi
+
+file=./$(date '+NEXTBIKE_PLACES_DB-%Y-%m-%d-%H:%M:%S.json')
+curl "$1" \
 	| grep 'var NEXTBIKE_PLACES_DB' \
 	| sed -e "s/^ *var NEXTBIKE_PLACES_DB = JSON.parse('//" -e "s/'); *\$//" \
 	> "$file"
